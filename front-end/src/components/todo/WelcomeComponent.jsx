5f84edaf101d111bba3dacbf7634b20af9b58fd4
kaki <cooolkaki@gmail.com>
2024-06-04 22:53:48 +0900

    feat: connect rest api

--- front-end/src/components/todo/WelcomeComponent.jsx
+++ front-end/src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,34 @@
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 function WelcomeComponent() {
 
   const { username } = useParams()
+
+  function callHelloWorldRestApi() {
+    console.log('called')
+
+    axios.get('http://localhost:8080/hello-world')
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log('cleanup'))
+
+    // retrieveHelloWorldBean()
+    //   .then((response) => successfulResponse(response))
+    //   .catch((error) => errorResponse(error))
+    //   .finally(() => console.log('cleanup'))
+
+  }
+
+  function successfulResponse(response) {
+    console.log(response)
+    //setMessage(response.data)
+    // setMessage(response.data.message)
+  }
+
+  function errorResponse(error) {
+    console.log(error)
+  }
 
   return (
     <div className="WelcomeComponent">
@@ -10,6 +36,11 @@ function WelcomeComponent() {
       <div>
         Manage your todos - <Link to="/todos">Go here</Link>
       </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call Hello World</button>
+      </div>
+      {/* <div className="text-info">{message}</div> */}
     </div>
   )
 }
--- rest-api/src/main/java/com/kaki/restapi/RestApiApplication.java
+++ rest-api/src/main/java/com/kaki/restapi/RestApiApplication.java
@@ -2,6 +2,9 @@ package com.kaki.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -10,4 +13,18 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	//http://localhost:3000/ to 8080
+	//Cross Origin Requests
+	//Allow all requests only from http://localhost:3000/
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
