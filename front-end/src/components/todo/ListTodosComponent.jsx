ca29472c16e45df010d2a1a0a0b28c4a541d2c8b
kaki <cooolkaki@gmail.com>
2024-06-05 21:52:37 +0900

    feat: retrieve todos: users/{username}/todos

--- front-end/src/components/todo/ListTodosComponent.jsx
+++ front-end/src/components/todo/ListTodosComponent.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react"
+import { retrieveAllTodosForUsername } from "./api/TodoApiService"
+
 function ListTodosComponent() {
 
   const today = new Date()
 
   const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
-  const todos = [
-    { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
-    { id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate },
-    { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate },
-  ]
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => refreshTodos(), [])
+
+  function refreshTodos() {
+
+    retrieveAllTodosForUsername('kaki')
+      .then(response => {
+        setTodos(response.data)
+      }
+
+      )
+      .catch(error => console.log(error))
+
+  }
 
 
   return (
@@ -32,7 +45,7 @@ function ListTodosComponent() {
                     <td>{todo.id}</td>
                     <td>{todo.description}</td>
                     <td>{todo.done.toString()}</td>
-                    <td>{todo.targetDate.toDateString()}</td>
+                    <td>{todo.targetDate.toString()}</td>
                   </tr>
                 )
               )
